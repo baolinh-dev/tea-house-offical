588f5eb6c7d39b37b71d3f9ee14483c34ad97f27
baolinh-dev <107604287+baolinh-dev@users.noreply.github.com>
2022-08-30 15:42:58 +0700

    update responsive

---
+++ assets/script/mobile.js
@@ -0,0 +1,12 @@
+var btnProductNav = document.querySelector('.toggle-btn-product-nav')  
+var navListProduct = document.querySelector('.nav-product-list') 
+var height
+btnProductNav.onclick = () => { 
+    if(navListProduct.clientHeight == 0) { 
+        navListProduct.style.height = "auto"; 
+    } else { 
+        navListProduct.style.height = "0px"; 
+    }
+}    
+console.log(navListProduct.clientHeight);
+
